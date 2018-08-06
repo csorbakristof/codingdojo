d7a9c9bb5b22eedddc58261c99c0e35dedf76112
Kristof <kristof@aut.bme.hu>
2018-08-06 11:03:33 +0200

    Refactored RotatingStroke to delegate.

--- 20180806SpirographLib/RotatingStroke.cs
+++ 20180806SpirographLib/RotatingStroke.cs
@@ -1,15 +1,22 @@
 ﻿using OpenCvSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _20180806SpirographLib
 {
     public class RotatingStroke : PolyStroke
     {
+        private PolyStroke delegateStroke;
+        public RotatingStroke(PolyStroke delegateStroke)
+        {
+            this.delegateStroke = delegateStroke;
+        }
+
         public void Rotate(double angularSpeed, double radius)
         {
-            var controlPoints = points;
-            points = new List<Point>();
+            var controlPoints = delegateStroke.ToList();
+            points.Clear();
             for (int i = 0; i < controlPoints.Count; i++)
             {
                 double x = Math.Round(controlPoints[i].X - radius * Math.Sin(i * angularSpeed / 180.0 * Math.PI));
--- 20180806SpirographLib/Stroke.cs
+++ 20180806SpirographLib/Stroke.cs
@@ -5,14 +5,5 @@ namespace _20180806SpirographLib
 {
     public interface Stroke : IEnumerable<Point>
     {
-/*        public virtual IEnumerator<Point> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        } */
     }
 }
