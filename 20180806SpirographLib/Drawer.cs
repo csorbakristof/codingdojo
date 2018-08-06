04fa088ce34276fabe6412688b53d0f4096d2c36
Kristof <kristof@aut.bme.hu>
2018-08-06 13:01:41 +0200

    Connecting drawing added.

--- 20180806SpirographLib/Drawer.cs
+++ 20180806SpirographLib/Drawer.cs
@@ -9,9 +9,30 @@ namespace _20180806SpirographLib
 {
     public class Drawer
     {
-        public void Draw(Mat img, Stroke stroke)
+        public void Draw(Mat img, Stroke stroke, bool connectPoints)
         {
-            var indexer = img.GetGenericIndexer<Vec3b>();
+            if (!connectPoints)
+                DrawPointByPoint(img, stroke);
+            else
+                DrawConnecting(img, stroke);
+        }
+
+        private void DrawConnecting(Mat img, Stroke stroke)
+        {
+            var points = stroke.ToArray();
+            for(int i=0; i<points.Length-1; i++)
+            {
+                Vec3b color = points[i].Color;
+                Cv2.Line(img,
+                    new Point(points[i].X, points[i].Y),
+                    new Point(points[i + 1].X, points[i + 1].Y),
+                    new Scalar(color.Item0, color.Item1, color.Item2));
+            }
+        }
+
+        private void DrawPointByPoint(Mat image, Stroke stroke)
+        {
+            var indexer = image.GetGenericIndexer<Vec3b>();
             foreach (var p in stroke)
                 indexer[p.Y, p.X] = p.Color;
         }
--- 20180806SpirographTests/DrawerTests.cs
+++ 20180806SpirographTests/DrawerTests.cs
@@ -11,18 +11,35 @@ namespace _20180806SpirographTests
     public class DrawerTests
     {
         [TestMethod]
-        public void BasicDrawing()
+        public void PointByPointDrawing()
+        {
+            Mat img = new Mat(100, 100, MatType.CV_8UC3, new Scalar(0, 0, 0));
+            var d = new Drawer();
+            Vec3b color = new Vec3b(128, 192, 255);
+            Vec3b black = new Vec3b(0, 0, 0);
+            var s = new PolyStroke(new SPoint[] {
+                new SPoint(10,20,color), new SPoint(20,20,color) });
+            d.Draw(img, s, false);
+            var indexer = img.GetGenericIndexer<Vec3b>();
+            Assert.AreEqual(black, indexer[10, 10]);
+            Assert.AreEqual(color, indexer[20, 10]);
+            Assert.AreEqual(black, indexer[20, 15]);
+            Assert.AreEqual(color, indexer[20, 20]);
+        }
+
+        [TestMethod]
+        public void ConnectingDrawing()
         {
             Mat img = new Mat(100, 100, MatType.CV_8UC3, new Scalar(0, 0, 0));
             var d = new Drawer();
             Vec3b color = new Vec3b(128, 192, 255);
             var s = new PolyStroke(new SPoint[] {
-                new SPoint(10,20,color), new SPoint(20,30,color) });
-            d.Draw(img, s);
+                new SPoint(10,10,color), new SPoint(20,10,color) });
+            d.Draw(img, s, true);
             var indexer = img.GetGenericIndexer<Vec3b>();
-            Assert.AreEqual(new Vec3b(0,0,0), indexer[10, 10]);
-            Assert.AreEqual(color, indexer[20, 10]);
-            Assert.AreEqual(color, indexer[30, 20]);
+            Assert.AreEqual(new Vec3b(0, 0, 0), indexer[50, 50]);
+            Assert.AreEqual(color, indexer[10, 10]);
+            Assert.AreEqual(color, indexer[10, 15]);
         }
     }
 }
