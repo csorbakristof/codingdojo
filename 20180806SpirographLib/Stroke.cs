ac83c79e6f7f6a84b3a48b8b65f0f6a5174c4566
Kristof <kristof@aut.bme.hu>
2018-08-06 10:06:13 +0200

    Stroke can store and enumerate points

--- 20180806SpirographLib/Stroke.cs
+++ 20180806SpirographLib/Stroke.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +8,22 @@ using OpenCvSharp;
 
 namespace _20180806SpirographLib
 {
-    public class Stroke
+    public class Stroke : IEnumerable<Point>
     {
+        private List<Point> points = new List<Point>();
         public void Add(Point point)
         {
+            points.Add(point);
+        }
+
+        public IEnumerator<Point> GetEnumerator()
+        {
+            return points.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return points.GetEnumerator();
         }
     }
 }
--- 20180806SpirographTests/StrokeTests.cs
+++ 20180806SpirographTests/StrokeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using _20180806SpirographLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenCvSharp;
@@ -9,10 +10,15 @@ namespace _20180806SpirographTests
     public class StrokeTests
     {
         [TestMethod]
-        public void TestStroke()
+        public void PointStorage()
         {
+            var p1 = new Point(0, 0);
+            var p2 = new Point(1, 1);
             var s = new Stroke();
-            s.Add(new Point(0, 0));
+            s.Add(p1);
+            s.Add(p2);
+            Assert.IsTrue(s.Any(i => i == p1));
+            Assert.IsTrue(s.Any(i => i == p2));
         }
     }
 }
