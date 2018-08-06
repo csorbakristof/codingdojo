85edd2fb051c817631ea53db14d613f7f38845a2
Kristof <kristof@aut.bme.hu>
2018-08-06 10:30:55 +0200

    Can rotate

--- 20180806SpirographLib/Stroke.cs
+++ 20180806SpirographLib/Stroke.cs
@@ -46,5 +46,17 @@ namespace _20180806SpirographLib
             for(int i=0; i< controlPoints.Count-1; i++)
                 AddPointsBetween(controlPoints[i], controlPoints[i + 1], (i!=0));
         }
+
+        public void Rotate(double angularSpeed, double radius)
+        {
+            var controlPoints = points;
+            points = new List<Point>();
+            for (int i = 0; i < controlPoints.Count; i++)
+            {
+                double x = Math.Round(controlPoints[i].X - radius * Math.Sin(i * angularSpeed / 180.0 * Math.PI));
+                double y = Math.Round(controlPoints[i].Y - radius * Math.Cos(i * angularSpeed / 180.0 * Math.PI));
+                points.Add(new Point(x, y));
+            }
+        }
     }
 }
--- 20180806SpirographTests/StrokeTests.cs
+++ 20180806SpirographTests/StrokeTests.cs
@@ -55,5 +55,29 @@ namespace _20180806SpirographTests
             Assert.IsTrue(s.Any(i => i == p3));
             Assert.AreEqual(21, s.Count());
         }
+
+        [TestMethod]
+        public void Rotate()
+        {
+            var p1 = new Point(0, 0);
+            var p2 = new Point(10, 0);
+            var p3 = new Point(20, 0);
+            var s = new Stroke();
+            s.Add(p1);
+            s.Add(p2);
+            s.Add(p3);
+            s.Rotate(90.0, 5.0);
+            Assert.AreEqual(3, s.Count());
+            var points = s.ToList();
+            Assert.IsTrue(s.Any(i => i == Shift(p1, 0, -5)));
+            Assert.IsTrue(s.Any(i => i == Shift(p2, -5, 0)));
+            Assert.IsTrue(s.Any(i => i == Shift(p3, 0, 5)));
+
+        }
+
+        private Point Shift(Point basePoint, int xShift, int yShift)
+        {
+            return new Point(basePoint.X + xShift, basePoint.Y + yShift);
+        }
     }
 }
