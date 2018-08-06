53f165d397fe8c0cade98f41d6cb0b74aa9cfebc
Kristof <kristof@aut.bme.hu>
2018-08-06 10:34:40 +0200

    Test tidy-up

--- 20180806SpirographTests/StrokeTests.cs
+++ 20180806SpirographTests/StrokeTests.cs
@@ -9,16 +9,17 @@ namespace _20180806SpirographTests
     [TestClass]
     public class StrokeTests
     {
+        Stroke stroke = new Stroke();
+
         [TestMethod]
         public void PointStorage()
         {
             var p1 = new Point(0, 0);
             var p2 = new Point(1, 1);
-            var s = new Stroke();
-            s.Add(p1);
-            s.Add(p2);
-            Assert.IsTrue(s.Any(i => i == p1));
-            Assert.IsTrue(s.Any(i => i == p2));
+            stroke.Add(p1);
+            stroke.Add(p2);
+            AssertPointPresence(p1);
+            AssertPointPresence(p2);
         }
 
         [TestMethod]
@@ -27,12 +28,11 @@ namespace _20180806SpirographTests
             var p1 = new Point(0, 0);
             var p2 = new Point(10, 0);
             var pTest = new Point(6, 0);
-            var s = new Stroke();
-            s.AddPointsBetween(p1, p2);
-            Assert.IsTrue(s.Any(i => i == p1));
-            Assert.IsTrue(s.Any(i => i == pTest));
-            Assert.IsTrue(s.Any(i => i == p2));
-            Assert.AreEqual(11, s.Count());
+            stroke.AddPointsBetween(p1, p2);
+            AssertPointPresence(p1);
+            AssertPointPresence(pTest);
+            AssertPointPresence(p2);
+            Assert.AreEqual(11, stroke.Count());
         }
 
         [TestMethod]
@@ -43,17 +43,16 @@ namespace _20180806SpirographTests
             var p2 = new Point(10, 0);
             var pTest2 = new Point(10, 4);
             var p3 = new Point(10, 10);
-            var s = new Stroke();
-            s.Add(p1);
-            s.Add(p2);
-            s.Add(p3);
-            s.Interpolate();
-            Assert.IsTrue(s.Any(i => i == p1));
-            Assert.IsTrue(s.Any(i => i == pTest1));
-            Assert.AreEqual(1, s.Count(i => i == p2));
-            Assert.IsTrue(s.Any(i => i == pTest2));
-            Assert.IsTrue(s.Any(i => i == p3));
-            Assert.AreEqual(21, s.Count());
+            stroke.Add(p1);
+            stroke.Add(p2);
+            stroke.Add(p3);
+            stroke.Interpolate();
+            AssertPointPresence(p1);
+            AssertPointPresence(pTest1);
+            Assert.AreEqual(1, stroke.Count(i => i == p2));
+            AssertPointPresence(pTest2);
+            AssertPointPresence(p3);
+            Assert.AreEqual(21, stroke.Count());
         }
 
         [TestMethod]
@@ -62,22 +61,26 @@ namespace _20180806SpirographTests
             var p1 = new Point(0, 0);
             var p2 = new Point(10, 0);
             var p3 = new Point(20, 0);
-            var s = new Stroke();
-            s.Add(p1);
-            s.Add(p2);
-            s.Add(p3);
-            s.Rotate(90.0, 5.0);
-            Assert.AreEqual(3, s.Count());
-            var points = s.ToList();
-            Assert.IsTrue(s.Any(i => i == Shift(p1, 0, -5)));
-            Assert.IsTrue(s.Any(i => i == Shift(p2, -5, 0)));
-            Assert.IsTrue(s.Any(i => i == Shift(p3, 0, 5)));
+            stroke.Add(p1);
+            stroke.Add(p2);
+            stroke.Add(p3);
+            stroke.Rotate(90.0, 5.0);
+            Assert.AreEqual(3, stroke.Count());
+            var points = stroke.ToList();
+            AssertPointPresence(Shift(p1, 0, -5));
+            AssertPointPresence(Shift(p2, -5, 0));
+            AssertPointPresence(Shift(p3, 0, 5));
 
         }
 
         private Point Shift(Point basePoint, int xShift, int yShift)
         {
             return new Point(basePoint.X + xShift, basePoint.Y + yShift);
+        }
+
+        private void AssertPointPresence(Point p)
+        {
+            Assert.IsTrue(stroke.Any(i => i == p));
         }
     }
 }
