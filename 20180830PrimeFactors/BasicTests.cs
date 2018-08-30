caa04fa79d6c1b00083507947e72cff2c6ad8b1c
Kristof <kristof@aut.bme.hu>
2018-08-30 09:38:19 +0200

    20180830PrimeFactors added, twisted with Linq

---
+++ 20180830PrimeFactors/BasicTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _20180829PrimeFactors
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void Start() => AssertEq(1, new int[] { 1 });
+
+        [TestMethod]
+        public void Test2() => AssertEq(2, new int[] { 2 });
+
+        [TestMethod]
+        public void Test4() => AssertEq(4, new int[] { 2, 2 });
+
+        [TestMethod]
+        public void Test6() => AssertEq(6, new int[] { 2, 3 });
+
+        [TestMethod]
+        public void FurtherTests()
+        {
+            AssertEq(5, new int[] { 5 });
+            AssertEq(9, new int[] { 3, 3 });
+            AssertEq(27, new int[] { 3, 3, 3 });
+            AssertEq(2*2*3*3*5*7*13, new int[] { 2, 2, 3, 3, 5, 7, 13 });
+        }
+
+        private void AssertEq(int num, int[] primeFactors)
+        {
+            var p = new PrimeFactors();
+            Assert.IsTrue(Enumerable.SequenceEqual(primeFactors, p.Factorize(num)));
+        }
+    }
+
+    internal class PrimeFactors
+    {
+        internal int[] Factorize(int v)
+        {
+            return Enumerable.Range(2, v).CollectExponents(v).ToArray();
+        }
+    }
+
+    static class Extensions
+    {
+        public static IEnumerable<int> CollectExponents(this IEnumerable<int> factors, int num)
+        {
+            if (num == 1)
+                yield return 1;
+            foreach (var f in factors)
+            {
+                while (num % f == 0)
+                {
+                    num /= f;
+                    yield return f;
+                }
+            }
+        }
+    }
+}
---
+++ 20180830PrimeFactors/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("20180829PrimeFactors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20180829PrimeFactors")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b615c6d-e6fa-4107-801f-68358453b627")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
