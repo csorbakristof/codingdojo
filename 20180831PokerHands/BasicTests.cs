0b0f45645bca6df3002477c7ab06db9bf1802b3f
Kristof <kristof@aut.bme.hu>
2018-08-31 12:45:47 +0200

    20180831PokerHands added (in progress)

---
+++ 20180831PokerHands/BasicTests.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _20180831PokerHands
+{
+    [TestClass]
+    public class BasicTests
+    {
+        // Clubs (treff), Diamonds (káró), Hearts (szív), Spades (pikk)
+        // 2, 3, 4, 5, 6, 7, 8, 9, T (ten), J (Jack), Q (Queen), K (King), A (Ace)
+        [TestMethod]
+        public void CanCompareValues()
+        {
+            var pairs = new(Card h, Card l)[]
+            {
+                (new Card("CA"), new Card("CK")),
+                (new Card("CJ"), new Card("CT")),
+                (new Card("C6"), new Card("C5")),
+                (new Card("C4"), new Card("C2")),
+                (new Card("CK"), new Card("C3")),
+                (new Card("CQ"), new Card("C7"))
+            };
+            foreach (var p in pairs)
+            {
+                Assert.IsTrue(p.l.CompareTo(p.h) < 0);
+                Assert.IsTrue(p.h.CompareTo(p.l) > 0);
+                Assert.IsTrue(p.l.CompareTo(p.l) == 0);
+                Assert.IsTrue(p.h.CompareTo(p.h) == 0);
+            }
+        }
+
+        [TestMethod]
+        public void CanParse()
+        {
+            Assert.AreEqual(new Card() { Suit = 'C', Value = 'T' }, new Card("CT"));
+        }
+
+        [TestMethod]
+        public void IsSameSuit()
+        {
+            Assert.IsTrue((new Card("CT")).SuitEquals(new Card("C8")));
+        }
+
+        [TestMethod]
+        public void CanParseAndOrderHand()
+        {
+            string hand = "C3 D6 HT D4 C2";
+            Card[] cards = Poker.GetHand(hand);
+            Assert.AreEqual(5, cards.Length);
+            Assert.AreEqual(new Card("HT"), cards[0]);
+            Assert.AreEqual(new Card("D6"), cards[1]);
+            Assert.AreEqual(new Card("D4"), cards[2]);
+            Assert.AreEqual(new Card("C3"), cards[3]);
+            Assert.AreEqual(new Card("C2"), cards[4]);
+        }
+
+        [TestMethod]
+        public void RecognizeHighestCard()
+        {
+            AssertEval("C2 C3 D4 D6 HT", (Poker.Rank.HighCards, "HT"));
+        }
+
+        const string testFlush = "C2 C3 C4 C6 CT";
+        [TestMethod]
+        public void RecognizeFlush()
+        {
+            AssertEval(testFlush, (Poker.Rank.Flush, "CT"));
+            Assert.AreNotEqual(Poker.Rank.Flush, Poker.Eval("C2 D3 C4 S6 CT").rank);
+        }
+
+        const string testStraight = "C7 D8 S9 ST CJ";
+        [TestMethod]
+        public void RecognizeStraight()
+        {
+            AssertEval(testStraight, (Poker.Rank.Straight, "CJ"));
+            Assert.AreNotEqual(Poker.Rank.Straight, Poker.Eval("C2 D3 S4 S6 CT").rank);
+        }
+
+
+        const string testPair = "C2 D3 S3 HJ HA";
+        [TestMethod]
+        public void RecognizePair()
+        {
+            AssertEval(testPair, (Poker.Rank.Pair, "3"));
+        }
+
+        const string testTwoPairs = "C2 D3 S3 HJ CJ";
+        [TestMethod]
+        [Ignore]
+        public void RecognizeTwoPairs()
+        {
+            AssertEval(testTwoPairs, (Poker.Rank.TwoPairs, "J"));
+        }
+
+        private void AssertEval(string hand, (Poker.Rank rank, string value) correct)
+        {
+            Assert.AreEqual(correct, Poker.Eval(hand));
+        }
+    }
+
+
+    internal static class Poker
+    {
+        public enum Rank
+        {
+            HighCards, Pair, TwoPairs, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush
+        }
+
+        static public (Rank rank, string value) Eval(string hand)
+        {
+            var h = GetHand(hand);
+            var rank = Rank.HighCards;
+            string value = h[0].ToString();
+            var v = IsPair(h);
+            if (v != null)
+            {
+                value = v;
+                rank = Rank.Pair;
+            }
+            if (IsStraight(h))
+                rank = Rank.Straight;
+            if (IsFlush(h))
+                rank = Rank.Flush;
+            return (rank, value);
+        }
+
+        private static string IsPair(Card[] h)
+        {
+            for (int i = 0; i < 4; i++)
+                if (h[i].Value == h[i + 1].Value)
+                    return h[i].Value.ToString();
+            return null;
+        }
+
+        private static bool IsStraight(Card[] h)
+        {
+            for (int i = 0; i < 4; i++)
+                if (h[i].CompareTo(h[i + 1]) != 1)
+                    return false;
+            return true;
+        }
+
+        private static bool IsFlush(Card[] cards)
+        {
+            for (int i = 1; i < 5; i++)
+                if (!cards[0].SuitEquals(cards[i]))
+                    return false;
+            return true;
+        }
+
+        internal static Card[] GetHand(string hand)
+        {
+            string[] cardStrings = hand.Split(' ');
+            var cards = cardStrings.Select(s => new Card(s)).OrderByDescending(c=>c).ToArray();
+            return cards;
+        }
+    }
+}
---
+++ 20180831PokerHands/Card.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _20180831PokerHands
+{
+    internal class Card : IComparable
+    {
+        private static char[] values = new char[] { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
+
+        public Card()
+        {
+        }
+
+        public Card(string card)
+        {
+            Suit = card[0];
+            Value = card[1];
+        }
+
+        public char Suit { get; set; }
+        public char Value { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Card o))
+                return false;
+            return (this.Suit == o.Suit && this.Value == o.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1625629942;
+            hashCode = hashCode * -1521134295 + Suit.GetHashCode();
+            hashCode = hashCode * -1521134295 + Value.GetHashCode();
+            return hashCode;
+        }
+
+        internal bool SuitEquals(Card other)
+        {
+            return (Suit == other.Suit);
+        }
+
+        internal static bool IsHigher(char valueH, char valueL)
+        {
+            int idxH = Array.IndexOf(values, valueH);
+            int idxL = Array.IndexOf(values, valueL);
+            return (idxH > idxL);
+        }
+
+        public int CompareTo(object obj)
+        {
+            // Positive return value means this > obj.
+            return Array.IndexOf(values, this.Value) -
+                Array.IndexOf(values, (obj as Card).Value);
+        }
+
+        public override string ToString()
+        {
+            return $"{Suit}{Value}";
+        }
+    }
+}
