52c72b9ad06c1f01b2057ba3220497f26b7f1b0e
Kristof <kristof@aut.bme.hu>
2018-09-05 17:13:29 +0200

    Bingo implementation started. Infinie loop is last option is in diagonal.

--- 20180905PeerReviewBingo/BasicTests.cs
+++ 20180905PeerReviewBingo/BasicTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace _20180905PeerReviewBingo
@@ -6,24 +7,65 @@ namespace _20180905PeerReviewBingo
     [TestClass]
     public class BasicTests
     {
+        readonly Bingo b = new Bingo();
         [TestMethod]
         public void BasicTest()
         {
-            var b = new Bingo();
-            var mtx = b.GetMatrix(0);
-            Assert.IsNull(mtx);
-        }
-    }
-
-    internal class Bingo
-    {
-        public Bingo()
-        {
+            const int peerCount = 4;
+            const int reviewerCount = 2;
+            var bc = new BingoChecker(reviewerCount);
+            b.random = new Random(1);
+            b.Init(peerCount);
+            b.ReviewNum = reviewerCount;
+            var m = b.GetMatrix();
+            Assert.IsTrue(bc.IsValid(m));
         }
 
-        internal object GetMatrix(int n)
+        [TestMethod]
+        public void AddingRow_UpdatesRowAndColSums()
         {
-            return null;
+            b.Init(10);
+            Assert.AreEqual(10, b.ColumnSum.Length);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(0, b.ColumnSum[i]);
+                Assert.AreEqual(0, b.RowSum[i]);
+            }
+            b.UpdateColumnCountersWithNewElement(2,3);
+            Assert.AreEqual(1, b.ColumnSum[3]);
+            Assert.AreEqual(1, b.RowSum[2]);
+            Assert.AreEqual(0, b.ColumnSum[2]);
+            Assert.AreEqual(0, b.RowSum[3]);
+            b.UpdateColumnCountersWithNewElement(2,3);
+            Assert.AreEqual(2, b.ColumnSum[3]);
+            Assert.AreEqual(2, b.RowSum[2]);
+            Assert.AreEqual(0, b.ColumnSum[2]);
+            Assert.AreEqual(0, b.RowSum[3]);
+        }
+
+        [TestMethod]
+        public void EnableMaskTest()
+        {
+            b.Init(5);
+            b.ColumnSum[1] = 1;
+            b.ColumnSum[2] = 2;
+            b.RowSum[1] = 1;
+            b.RowSum[2] = 2;
+            Assert.IsTrue(b.IsEnabled(1, 1, 2));
+            Assert.IsFalse(b.IsEnabled(2, 2, 2));
+        }
+
+        [TestMethod]
+        public void RandomizeLocationsToEnabledPlaces()
+        {
+            b.Init(5);
+            b.ReviewNum = 2;
+            b.ColumnSum = new int[5] { 2, 1, 2, 2, 2 };
+            b.RowSum = new int[5] { 2, 1, 2, 2, 2 };
+            b.random = new Random(1);
+            var location = b.GetRandomAllowedLocation();
+            Assert.AreEqual(1, location.row);
+            Assert.AreEqual(1, location.col);
         }
     }
 }
---
+++ 20180905PeerReviewBingo/Bingo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _20180905PeerReviewBingo
+{
+    internal class Bingo
+    {
+        public Bingo()
+        {
+        }
+
+        public Random random = new Random();
+
+        public int[] ColumnSum { get; internal set; }
+        public int[] RowSum { get; internal set; }
+        public bool[,] Matrix { get; set; }
+
+        public int PeerNum { get; set; }
+        public int ReviewNum { get; set; }
+
+        internal void UpdateColumnCountersWithNewElement(int row, int col)
+        {
+            ColumnSum[col]++;
+            RowSum[row]++;
+        }
+
+        internal bool[,] GetMatrix()
+        {
+            for(int n=0; n<PeerNum*ReviewNum; n++)
+            {
+                (int r, int c) = GetRandomAllowedLocation();
+                Matrix[r, c] = true;
+                UpdateColumnCountersWithNewElement(r, c);
+            }
+            return Matrix;
+        }
+
+        internal bool IsEnabled(int row, int col, int reviewCount)
+        {
+            return (ColumnSum[col] < reviewCount && RowSum[row] < reviewCount
+                && !Matrix[row,col]
+                && row != col);
+        }
+
+        internal (int row, int col) GetRandomAllowedLocation()
+        {
+            if (ReviewNum == 0)
+                throw new ArgumentException("Bingo.ReviewNum cannot be zero.");
+            var n = PeerNum;
+            int i = random.Next(n*n);
+            while(!IsEnabled(i/n, i%n, ReviewNum))
+                i = ++i % (n * n);
+            return (row: i/n, col: i%n);
+        }
+
+        internal void Init(int n)
+        {
+            PeerNum = n;
+            ColumnSum = new int[n];
+            RowSum = new int[n];
+            Matrix = new bool[n, n];
+        }
+    }
+}
