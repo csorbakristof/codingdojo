a46076a96e97a7065d9fa54670439d95d816e11d
Kristof <kristof@aut.bme.hu>
2018-10-17 17:01:33 +0200

    Mixed ListBox with type-spefic command binding works.

--- 20181017UwpAndMixedListbox/RotatingStroke.cs
+++ 20181017UwpAndMixedListbox/RotatingStroke.cs
@@ -1,9 +1,50 @@
 ﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
 namespace _20181017UwpAndMixedListbox
 {
-    public class RotatingStroke : Stroke
+    public class RotatingStroke : Stroke, INotifyPropertyChanged
     {
-        public int RotationCount { get; set; }
+        private int rotationCount = 1;
+        public int RotationCount {
+            get => rotationCount;
+            set
+            {
+                if (rotationCount != value)
+                {
+                    rotationCount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RotationCount"));
+                }
+            }
+        }
+        public ICommand IncreaseCountCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RotatingStroke()
+        {
+            IncreaseCountCommand = new IncreaseCount() { Model = this };
+        }
+
+        public class IncreaseCount : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+
+            public RotatingStroke Model { get; set; }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                Model.RotationCount++;
+            }
+        }
+
     }
 }
